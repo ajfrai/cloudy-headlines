639062145f1e3dcb1c70de2786839c7321bea3bb
Abraham Fraifeld <ajfrai.all@gmail.com>
2021-04-04 02:47:44 -0400

    api key

--- trigger_grab_headllines_lambda.py
+++ trigger_grab_headllines_lambda.py
@@ -1,4 +1,4 @@
 import requests 
 
 requests.get('https://x8p4l9hrib.execute-api.us-east-1.amazonaws.com/default/grab-headlines',
-	headers = {'x-api-key':'PfB3hcxPC79U8DH2KPgQR3e6Lmn9b3o38hz5rHV6'}).text
+	headers = {'x-api-key':os.environ['lambda_api_key']}).text
